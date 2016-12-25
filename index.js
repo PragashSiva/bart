27c064aedf0817ebbd20533351ecbe256f7f5979
PragashSiva <pragash.acci@gmail.com>
2016-12-25 17:22:32 -0500

    Added a callback function

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ try {
 
 
 const client = new Wit({accessToken: WIT_TOKEN,actions});
-//interactive(client);
+client.interactive();
 
 function getName(context,entities){
 	console.log("YAY THIS WAS CALLED");
