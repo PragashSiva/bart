af855ab6de6656db8406085d4a10cb8b6a0c1e70
PragashSiva <pragash.acci@gmail.com>
2016-12-25 17:37:26 -0500

    trying something new

--- const.js
+++ const.js
@@ -9,13 +9,7 @@ if (!WIT_TOKEN) {
 // Messenger API parameters
 const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 
-var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
-if (!FB_VERIFY_TOKEN) {
-  FB_VERIFY_TOKEN = "just_do_it";
-}
-
 module.exports = {
   WIT_TOKEN: WIT_TOKEN,
   FB_PAGE_TOKEN: FB_PAGE_TOKEN,
-  FB_VERIFY_TOKEN: FB_VERIFY_TOKEN,
 };
