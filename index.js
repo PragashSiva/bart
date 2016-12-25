bf3d6834f0d5cab50e21aa0d6092922bf846dd27
PragashSiva <pragash.acci@gmail.com>
2016-12-25 05:55:18 -0500

    test

--- index.js
+++ index.js
@@ -87,11 +87,11 @@ function sendGenericMessage(sender) {
             continue
         }
 
-        client.message(text.substring(0,200), {})
+        client.converse(text.substring(0,200), {})
 		.then((data) => {
 		  console.log('Yay, got Wit.ai response: ' + JSON.stringify(data))
 		  let reply = JSON.stringify(data)
-		  sendTextMessage(sender, "New text received, echo: " + reply.substring(0, 200))
+		  sendTextMessage(sender, "New text received, echo: " + reply.substring(0,200))
 		})
 		.catch(console.error);
 
