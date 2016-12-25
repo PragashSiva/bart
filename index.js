929ff115005bd84f43a932942aa2ad763b8d30a0
PragashSiva <pragash.acci@gmail.com>
2016-12-25 06:34:40 -0500

    test

--- index.js
+++ index.js
@@ -100,6 +100,7 @@ const findOrCreateSession = (fbid) => {
     for (let i = 0; i < messaging_events.length; i++) {
       let event = req.body.entry[0].messaging[i]
       let sender = event.sender.id
+      const sessionId = findOrCreateSession(sender);
       if (event.message && event.message.text) {
         let text = event.message.text
         if (text === 'Generic') {
